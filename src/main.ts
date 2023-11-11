1f90d5efbb9f07a16e9aae1f71b9e3e48754e499
Gabriele Alfarano <gabalpha@outlook.it>
2023-11-12 00:38:37 +0100

    feat: Init btn create github issue

--- src/main.ts
+++ src/main.ts
@@ -25,14 +25,15 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		const isOnMarkdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const syncBtn = this.addRibbonIcon('upload', 'Create a github issue', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
+		syncBtn.style.display = isOnMarkdownView ? 'inherit' : 'none';
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
@@ -60,8 +61,7 @@ export default class MyPlugin extends Plugin {
 			name: 'Open sample modal (complex)',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
+				if (isOnMarkdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
@@ -80,11 +80,13 @@ export default class MyPlugin extends Plugin {
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			syncBtn.style.display = this.app.workspace.getActiveViewOfType(MarkdownView)
+				? 'inherit'
+				: 'none';
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {}
