589f00b5f30ec5fa8ff0f61ae1e2930f74d5c525
Gabriele Alfarano <gabalpha@outlook.it>
2023-11-13 20:05:24 +0100

    chore: Clean-up from sample-plugin example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import {
 	App,
-	Editor,
 	MarkdownFileInfo,
 	MarkdownView,
-	Modal,
 	Notice,
 	Plugin,
 	PluginSettingTab,
@@ -26,7 +24,7 @@ const DEFAULT_SETTINGS: GitHubIssueEditorSettings = {
 	repo: ''
 };
 
-export default class MyPlugin extends Plugin {
+export default class GithubIssueEditor extends Plugin {
 	settings: GitHubIssueEditorSettings;
 
 	async onload() {
@@ -34,95 +32,50 @@ export default class MyPlugin extends Plugin {
 
 		const isOnMarkdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 
-		// This creates an icon in the left ribbon.
-		const syncBtn = this.addRibbonIcon(
-			'upload',
-			'Create a github issue',
-			async (evt: MouseEvent) => {
-				const file = this.app.workspace.activeEditor as MarkdownFileInfo & { data: string };
-				const issueId = readIssueId(file.data);
+		const syncBtn = this.addRibbonIcon('upload', 'Create a github issue', async () => {
+			const file = this.app.workspace.activeEditor as MarkdownFileInfo & { data: string };
+			const issueId = readIssueId(file.data);
 
-				if (issueId) {
-					const res = await requestUrl({
-						url: `https://api.github.com/repos/${this.settings.owner}/${this.settings.repo}/issues/${issueId}`,
-						headers: { Authorization: `Bearer ${this.settings.token}` },
-						method: 'PATCH',
-						body: JSON.stringify({
-							title: file.file?.basename ?? '',
-							body: removeProperties(file.data)
-						})
-					});
-
-					if (res.status === 200) {
-						new Notice('Issue successfully updated!');
-					}
-					return;
-				}
-
+			if (issueId) {
 				const res = await requestUrl({
-					url: `https://api.github.com/repos/${this.settings.owner}/${this.settings.repo}/issues`,
+					url: `https://api.github.com/repos/${this.settings.owner}/${this.settings.repo}/issues/${issueId}`,
 					headers: { Authorization: `Bearer ${this.settings.token}` },
-					method: 'POST',
+					method: 'PATCH',
 					body: JSON.stringify({
 						title: file.file?.basename ?? '',
 						body: removeProperties(file.data)
 					})
 				});
 
-				if (res.status === 201) {
-					new Notice('Issue successfully created');
-					const propertiesWithGithubIssue = writeIssueId(file.data!, res.json.number);
-					this.app.vault.modify(
-						file.file!,
-						`${propertiesWithGithubIssue}\n${removeProperties(file.data)}`
-					);
+				if (res.status === 200) {
+					new Notice('Issue successfully updated!');
 				}
+				return;
 			}
-		);
+
+			const res = await requestUrl({
+				url: `https://api.github.com/repos/${this.settings.owner}/${this.settings.repo}/issues`,
+				headers: { Authorization: `Bearer ${this.settings.token}` },
+				method: 'POST',
+				body: JSON.stringify({
+					title: file.file?.basename ?? '',
+					body: removeProperties(file.data)
+				})
+			});
+
+			if (res.status === 201) {
+				new Notice('Issue successfully created');
+				const propertiesWithGithubIssue = writeIssueId(file.data!, res.json.number);
+				this.app.vault.modify(
+					file.file!,
+					`${propertiesWithGithubIssue}\n${removeProperties(file.data)}`
+				);
+			}
+		});
 
 		syncBtn.style.display = isOnMarkdownView ? 'inherit' : 'none';
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				if (isOnMarkdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new SettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -147,26 +100,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingTab extends PluginSettingTab {
+	plugin: GithubIssueEditor;
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GithubIssueEditor) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
