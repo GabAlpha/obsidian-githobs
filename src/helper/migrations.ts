9923554ee4b71967d02518f458d907bf0979b5e2
Gabriele Alfarano <gabalpha@outlook.it>
2025-08-14 13:37:26 +0200

    Migration logic fix for first time

--- src/helper/migrations.ts
+++ src/helper/migrations.ts
@@ -1,14 +1,21 @@
 import { supportMultipleRepos } from 'migrations/supportMultipleRepos';
-import { GitHobsSettings } from 'settings';
+import { DEFAULT_SETTINGS, GitHobsSettings } from 'settings';
 
 export type GenericSettings = Record<string, unknown> & { version: number };
 
 const MIGRATIONS: Array<(settings: GenericSettings) => GenericSettings> = [supportMultipleRepos];
 
-export function migrate(settings: GenericSettings): {
+export function migrate(settings: GenericSettings | undefined): {
 	newSettings: GitHobsSettings;
 	migrationsApplied: boolean;
 } {
+	// If this is your first time opening githobs
+	if (!settings || Object.keys(settings).length === 0) {
+		return {
+			newSettings: DEFAULT_SETTINGS,
+			migrationsApplied: true
+		};
+	}
 	const migrationsToApply = MIGRATIONS.splice(settings.version ?? 0);
 
 	if (migrationsToApply.length === 0) {
